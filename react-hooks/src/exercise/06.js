2b505f10366970cd4ae9d554f9af70fe2863dd4c
bara495 <barra.495@gmail.com>
2020-11-05 18:22:08 +0100

    bonus 2

--- react-hooks/src/exercise/06.js
+++ react-hooks/src/exercise/06.js
@@ -10,36 +10,53 @@ import {
   PokemonDataView,
 } from '../pokemon'
 
+const states = ['idle', 'pending', 'resolved', 'rejected']
+const idle = states[0]
+const pending = states[1]
+const resolved = states[2]
+const rejected = states[3]
+
 function PokemonInfo({pokemonName}) {
   const [pokemon, setPokemon] = React.useState(null)
   const [error, setError] = React.useState(null)
+  const [status, setStatus] = React.useState(idle)
 
   React.useEffect(() => {
     if (pokemonName === '') return
 
-    setPokemon(null)
+    // setError(null)
+    // setPokemon(null)
+
+    setStatus(pending)
 
     fetchPokemon(pokemonName).then(
-      pokemonData => setPokemon(pokemonData),
-      error => setError(error),
+      pokemonData => {
+        setPokemon(pokemonData)
+        setStatus(resolved)
+      },
+      error => {
+        setError(error)
+        setStatus(rejected)
+      },
     )
   }, [pokemonName])
 
-  if (error)
-    return (
-      <div role="alert">
-        There was an error:{' '}
-        <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
-      </div>
-    )
-
-  return !pokemonName ? (
-    'Submit a pokemon'
-  ) : !pokemon ? (
-    <PokemonInfoFallback name={pokemonName} />
-  ) : (
-    <PokemonDataView pokemon={pokemon} />
-  )
+  switch (status) {
+    case idle:
+      return 'Submit a pokemon'
+    case pending:
+      return <PokemonInfoFallback name={pokemonName} />
+    case rejected:
+      return (
+        <div role="alert">
+          There was an error:{' '}
+          <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+        </div>
+      )
+    case resolved:
+      return <PokemonDataView pokemon={pokemon} />
+      throw new Error('This should be impossible')
+  }
 }
 
 function App() {
