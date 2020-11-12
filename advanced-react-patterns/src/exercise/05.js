b9b974226dc40f72355685e56de74cf85a659591
bara495 <barra.495@gmail.com>
2020-11-12 15:53:44 +0100

    bonus 2

--- advanced-react-patterns/src/exercise/05.js
+++ advanced-react-patterns/src/exercise/05.js
@@ -4,14 +4,19 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
+const actionTypes = {
+  toggle: 'toggle',
+  reset: 'reset',
+}
+
 const callAll = (...fns) => (...args) => fns.forEach(fn => fn?.(...args))
 
 function toggleReducer(state, {type, initialState}) {
   switch (type) {
-    case 'toggle': {
+    case actionTypes.toggle: {
       return {on: !state.on}
     }
-    case 'reset': {
+    case actionTypes.reset: {
       return initialState
     }
     default: {
@@ -25,8 +30,8 @@ function useToggle({initialOn = false, reducer = {toggleReducer}} = {}) {
   const [state, dispatch] = React.useReducer(reducer, initialState)
   const {on} = state
 
-  const toggle = () => dispatch({type: 'toggle'})
-  const reset = () => dispatch({type: 'reset', initialState})
+  const toggle = () => dispatch({type: actionTypes.toggle})
+  const reset = () => dispatch({type: actionTypes.reset, initialState})
 
   function getTogglerProps({onClick, ...props} = {}) {
     return {
@@ -58,13 +63,13 @@ function App() {
 
   // function toggleStateReducer(state, action) {
   //   switch (action.type) {
-  //     case 'toggle': {
+  //     case actionTypes.toggle: {
   //       if (clickedTooMuch) {
   //         return {on: state.on}
   //       }
   //       return {on: !state.on}
   //     }
-  //     case 'reset': {
+  //    case actionTypes.reset: {
   //       return {on: false}
   //     }
   //     default: {
@@ -74,7 +79,7 @@ function App() {
   // }
 
   function toggleStateReducer(state, action) {
-    if (action.type === 'toggle' && clickedTooMuch) {
+    if (action.type === actionTypes.toggle && clickedTooMuch) {
       return {on: state.on}
     }
     return toggleReducer(state, action)
